7d00589e988e38adc44209acb112e6ce19797bb5
Russell Mair <russell.e.mair@gmail.com>
2021-12-08 20:55:35 +0000

    continuing mentor session, added urls new and added redirect

--- express-server.js
+++ express-server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -20,6 +24,12 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }
   res.render('urls_index', templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  urlDatabase["abc"] = req.body.longURL
+  res.redirect("/urls");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get('/u/:shortURL', (req, res)=> {
