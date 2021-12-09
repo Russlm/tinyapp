cdfd3c405c549e39fef12de1bdc0d4660b4ab465
Russell Mair <russell.e.mair@gmail.com>
2021-12-09 03:13:02 +0000

    added in delete buttons

--- express-server.js
+++ express-server.js
@@ -13,19 +13,26 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// const newUserID = generateRandomID();
+
+
+//SENDS USER HELLO
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// SENDS USER STANDARD HELLO WORLD 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//SENDS USERS THE URLS DATABASE 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }
   res.render('urls_index', templateVars);
 });
 
+//SENDS URERS THE URLS DATABASE 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   urlDatabase["abc"] = req.body.longURL
@@ -47,6 +54,12 @@ app.get('/u/:shortURL', (req, res)=> {
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
+// app.post("/urls/:shortURL/delete", (req, res) => {
+//   let shortURL = req.params.shortURL;
+//   let longURL = urlDatabase[shortURL]
+//   delete longURL;
+// });
 
 app.get('/urls/:shortURL', (req, res)=> {
   // res.send('You requested to see ' + urlDatabase[req.params.shortURL])
